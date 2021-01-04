71052ca447475769fe3bd3cf1037c2ab47172da8
gurvick <gghai02@gmail.com>
2021-01-04 00:01:25 -0500

    added social icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { FaLinkedin, FaGithub, FaYoutube } from 'react-icons/fa'
 import {
   FooterContainer,
-  FooterLink,
-  FooterLinkItems,
+  FooterWrap,
   FooterLinksContainer,
   FooterLinksWrapper,
+  FooterLinkItems,
   FooterLinkTitle,
-  FooterWrap,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
 } from './FooterElements'
 
 const Footer = () => {
@@ -17,18 +24,46 @@ const Footer = () => {
           <FooterLinksWrapper>
             <FooterLinkItems>
               <FooterLinkTitle>About</FooterLinkTitle>
-              <FooterLink to='/signin'>About us</FooterLink>
+              <FooterLink to='/'>About us</FooterLink>
               <FooterLink to='/signin'>Sign in</FooterLink>
-              <FooterLink to='/signin'>Github</FooterLink>
+              <FooterLink to='/'>Github</FooterLink>
             </FooterLinkItems>
             <FooterLinkItems>
               <FooterLinkTitle>Contact</FooterLinkTitle>
-              <FooterLink to='/signin'>Email</FooterLink>
-              <FooterLink to='/signin'>Phone</FooterLink>
-              <FooterLink to='/signin'>LinkedIn</FooterLink>
+              <FooterLink to='/'>Email</FooterLink>
+              <FooterLink to='/'>Phone</FooterLink>
+              <FooterLink to='/'>LinkedIn</FooterLink>
+              <FooterLink to='/'>YouTube</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>ezLoad</SocialLogo>
+            <WebsiteRights>
+              ezLoad © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink
+                href='//www.github.com/gurvick/ezLoad'
+                target='_blank'
+                aria-label='Github'
+              >
+                <FaGithub />
+              </SocialIconLink>
+              <SocialIconLink
+                href='//www.linkedin.com/in/gurvick-ghai'
+                target='_blank'
+                aria-label='Linkedin'
+              >
+                <FaLinkedin />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='YouTube'>
+                <FaYoutube />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
     </FooterContainer>
   )
