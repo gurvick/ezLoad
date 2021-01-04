5dfce0fc851b3e084e3d67212d8c244d440fcc29
gurvick <gghai02@gmail.com>
2021-01-04 15:40:01 -0500

    set up express and mongoose

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+// Set up express
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`))
+
+// Set up mongoose
+
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  },
+  (err) => {
+    if (err) throw err
+    console.log('MongoDB connection established')
+  }
+)
