d3693b2345562ffc37f2abef0d323e26cca9839a
gurvick <gghai02@gmail.com>
2021-01-04 01:59:42 -0500

    added additional scrolls

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -38,6 +38,11 @@ const HeroSection = () => {
               onMouseLeave={onHover}
               primary='true'
               dark='true'
+              smooth={true}
+              duration={500}
+              spy={true}
+              exact='true'
+              offset={-80}
             >
               Get started {hover ? <ArrowForward /> : <ArrowRight />}
             </Button>
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import ScrollToTop from '../components/ScrollToTop'
 import Signin from '../components/Signin'
 
 const SigninPage = () => {
   return (
-    <div>
+    <>
+      <ScrollToTop />
       <Signin />
-    </div>
+    </>
   )
 }
 
