ddf5dff773f7cf14afe837ce951e288dfa74b1b2
gurvick <gghai02@gmail.com>
2021-01-07 02:38:08 -0500

    added ternary operator for sidebar login

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,42 +1,61 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import UserContext from '../../context/UserContext'
+import { useHistory } from 'react-router-dom'
 import {
- SidebarContainer,
- Icon,
- CloseIcon,
- SidebarWrapper,
- SidebarMenu,
- SidebarLink,
- SideBtnWrap,
- SidebarRoute
-} from './SidebarElements';
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from './SidebarElements'
 
-const Sidebar = ({isOpen, toggle}) => {
- return (
-  <SidebarContainer isOpen={isOpen} onClick={toggle} >
-   <Icon onClick={toggle} >
-    <CloseIcon />
-   </Icon>
-   <SidebarWrapper>
-    <SidebarMenu>
-     <SidebarLink to="about" onClick={toggle}>
-      About
-     </SidebarLink>
-     <SidebarLink to="discover" onClick={toggle}>
-      Discover
-     </SidebarLink>
-     <SidebarLink to="services" onClick={toggle}>
-      Services
-     </SidebarLink>
-     <SidebarLink to="signup" >
-      Sign Up
-     </SidebarLink>
-    </SidebarMenu>
-    <SideBtnWrap>
-     <SidebarRoute to='/signin'>Sign In</SidebarRoute>
-    </SideBtnWrap>
-   </SidebarWrapper>
-  </SidebarContainer>
- )
+const Sidebar = ({ isOpen, toggle }) => {
+  const { userData, setUserData } = useContext(UserContext)
+  const history = useHistory()
+  const signup = () => history.push('/signup')
+  const signin = () => history.push('/signin')
+  const logout = () => {
+    setUserData({
+      token: undefined,
+      user: undefined,
+    })
+    localStorage.setItem('auth-token', '')
+  }
+  ;<SidebarRoute onClick={signin}>Sign In</SidebarRoute>
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to='about' onClick={toggle}>
+            About
+          </SidebarLink>
+          <SidebarLink to='discover' onClick={toggle}>
+            Discover
+          </SidebarLink>
+          <SidebarLink to='services' onClick={toggle}>
+            Services
+          </SidebarLink>
+          <SidebarLink to='signup'>Sign Up</SidebarLink>
+        </SidebarMenu>
+        {userData.user ? (
+          <SideBtnWrap>
+            <SidebarRoute onClick={logout}>Log Out</SidebarRoute>
+          </SideBtnWrap>
+        ) : (
+          <SideBtnWrap>
+            <SidebarRoute onClick={signup}>Sign Up</SidebarRoute>
+            <SidebarRoute onClick={signin}>Sign In</SidebarRoute>
+          </SideBtnWrap>
+        )}
+      </SidebarWrapper>
+    </SidebarContainer>
+  )
 }
 
 export default Sidebar
