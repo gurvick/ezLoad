a94f7cf1626eb886f378bdee2464d5d2b334193c
gurvick <gghai02@gmail.com>
2021-01-07 01:10:40 -0500

    added ternary operator for hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import Video from '../../video/video.mp4'
 import { Button } from '../ButtonElement'
+import UserContext from '../../context/UserContext'
+import { useHistory } from 'react-router-dom'
 import {
   HeroContainer,
   HeroBg,
@@ -18,6 +20,9 @@ const HeroSection = () => {
   const onHover = () => {
     setHover(!hover)
   }
+  const { userData, setUserData } = useContext(UserContext)
+  const history = useHistory()
+  const postings = () => history.push('/postings')
 
   return (
     <>
@@ -27,26 +32,50 @@ const HeroSection = () => {
         </HeroBg>
         <HeroContent>
           <HeroH1>Find Your Next Load</HeroH1>
-          <HeroP>
-            Sign up and join the easiest and fastest truck and load board
-            platform.
-          </HeroP>
-          <HeroBtnWrapper>
-            <Button
-              to='signup'
-              onMouseEnter={onHover}
-              onMouseLeave={onHover}
-              primary='true'
-              dark='true'
-              smooth={true}
-              duration={500}
-              spy={true}
-              exact='true'
-              offset={-80}
-            >
-              Get started {hover ? <ArrowForward /> : <ArrowRight />}
-            </Button>
-          </HeroBtnWrapper>
+          {userData.user ? (
+            <>
+              <HeroP>Click View Postings to find a load near you</HeroP>
+              <HeroBtnWrapper>
+                <Button
+                  onClick={postings}
+                  onMouseEnter={onHover}
+                  onMouseLeave={onHover}
+                  primary='true'
+                  dark='true'
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact='true'
+                  offset={-80}
+                >
+                  View Postings {hover ? <ArrowForward /> : <ArrowRight />}
+                </Button>
+              </HeroBtnWrapper>
+            </>
+          ) : (
+            <>
+              <HeroP>
+                Sign up and join the easiest and fastest truck and load board
+                platform.
+              </HeroP>
+              <HeroBtnWrapper>
+                <Button
+                  to='signup'
+                  onMouseEnter={onHover}
+                  onMouseLeave={onHover}
+                  primary='true'
+                  dark='true'
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact='true'
+                  offset={-80}
+                >
+                  Get started {hover ? <ArrowForward /> : <ArrowRight />}
+                </Button>
+              </HeroBtnWrapper>
+            </>
+          )}
         </HeroContent>
       </HeroContainer>
     </>
